7fd5a26bf0a0e936e66fab0716b7eedd619c7c12
Alice Hillier <alice_hillier@hotmail.com>
2023-10-19 15:15:16 +0100

    Adds code for sending mail once contact form complete.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function sendMail(contactForm) {
+    emailjs.send("service_wk1b7gb", "template_2odmt7e", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS");
+            },
+            function (error) {
+                console.log("FAILED", error);
+            });
+}
